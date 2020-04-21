c6d1bcc53437b7523a99de1d62b51bba56a12608
杜科 <1264310546@qq.com>
2020-04-21 16:03:28 +0800

    优化注释和readme

--- client/src/main/java/com/github/AllenDuke/clientService/Connector.java
+++ client/src/main/java/com/github/AllenDuke/clientService/Connector.java
@@ -35,7 +35,7 @@ public class Connector {
 
     /**
      * 已与某个RPCServer连接的RPCClientHandler，key为服务名称，如Calculator，但是有可能出现下一次要调用HelloService的时候
-     * 从此map中找不到RPCClient，而事实上Calculaor所在的RPCServer也提供了HelloService的服务，但无法感知到，从而又再次与此
+     * 从此map中找不到RPCClientHandler,而事实上Calculaor所在的RPCServer也提供了HelloService的服务,但无法感知到,从而又再次与此
      * 服务端再构建了一条连接，TODO 这个问题将在下一次更新中解决（在通信的时候服务端返回它能提供的所有服务），或者不解决。
      * 注意为什么不用ConcurrentHashMap，在createAndConnectChannel有解释
      */
@@ -58,7 +58,7 @@ public class Connector {
     //记录某种服务调用超时的主机名单，用于下次调用时过滤
     private static final Map<String, Set<String>> timeOutMap = new HashMap<>();
 
-    //复用bootstrap，之前一个连接对应一个bootstrap和 一个nioeventloopgrouph和一个nioeventloop，
+    //复用bootstrap，之前一个连接对应一个bootstrap和 一个nioeventloopgroup和一个nioeventloop，
     //现在复用了线程，发挥了nio的优势，但并没有复用空闲的tcp的连接
     private static Bootstrap bootstrap;
     private static NioEventLoopGroup group;
